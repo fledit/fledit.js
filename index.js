c691400e6f889c04a1ee22550d964203de9646d9
pirhoo <pierre.romera@gmail.com>
2015-05-02 22:57:45 -0400

    Switched to superagent

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var request = require('request'),
+var request = require('superagent'),
      events = require('events');
 
 function File(id) {
@@ -8,8 +8,9 @@ function File(id) {
   var url = "http://localhost:9000/api/files/" + id,
   emitter = new events.EventEmitter();
 
-  request({ url: url, json: true }, function(err, res, body) {
-    emitter.emit("complete", body);
+  request.get(url).end(function(err, res) {
+    console.log(res.body);
+    emitter.emit("complete", res.body);
   });
 
   return emitter;
