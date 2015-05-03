48c48e8e64a5d651b0213a69e57a57c4845e9bf7
pirhoo <pierre.romera@gmail.com>
2015-05-03 09:21:35 -0400

    Added file object

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ var request = require('superagent'),
      events = require('events');
 
 function File(id) {
-
+  // Build the URL to the file
   var url = "http://localhost:9000/api/files/" + id,
-  emitter = new events.EventEmitter();
+  // Create an event emitter
+  emitter = new events.EventEmitter(),
+  // Create an empty object to contain the futur file
+  file = null;
+
 
   request.get(url).end(function(err, res) {
     if(res.ok) {
@@ -14,7 +18,6 @@ function File(id) {
     } else {
       emitter.emit("error", err);
     }
-
   });
 
   return emitter;
