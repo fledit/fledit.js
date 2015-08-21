ac7552e8ddba08a55b004128fdf4ae79abc16b68
pirhoo <hello@pirhoo.com>
2015-08-21 20:15:20 +0200

    Rename bower task to browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,5 +52,5 @@ module.exports = function (grunt) {
   });
 
   grunt.registerTask('default', ['jshint', 'mochacli']);
-  grunt.registerTask('bower', ['browserify', 'uglify']);
+  grunt.registerTask('browser', ['browserify', 'uglify']);
 };
